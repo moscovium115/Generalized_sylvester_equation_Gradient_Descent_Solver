46ee795e9bca70fedf3ccbc30d95628ee9dfc511
Anis el El Hachmioui <34982326+moscovium115@users.noreply.github.com>
2023-07-20 16:28:35 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,10 @@
 import numpy as np
+from GD_solver import *
 
-Mat_1=[np.array([[1,2],[3,4]]),np.array([[5,6],[7,8]])]
-Mat_2=[np.array([[1,2],[3,4]]),np.array([[5,6],[7,10]])]
+Mat_1=[np.array([[1,2],[3,4]]),np.eye(2),np.array([[5,6],[7,8]])]
+Mat_2=[np.eye(2),np.array([[5,6],[7,10]]),np.array([[1,2],[3,4]])]
+iterations=100
+E=np.array([[3,8],[9,10]])
+solution_matrix=Gen_Sylv_gradient_descent(Mat_1,Mat_2,E,iterations)
+print(Sylvester_Product(Mat_1,Mat_2,solution_matrix))
+print(E)
